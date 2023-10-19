9d8432e90ab31ae4a45f1c4ba3772cd435d01265
BryanRF <92905668+BryanRF@users.noreply.github.com>
2023-10-19 00:21:15 -0500

    cambios ultimos

--- proyecto_mascotas_django/api/urls.py
+++ proyecto_mascotas_django/api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from .views import PersonaView, MascotaView
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('personas/', PersonaView.as_view(), name='persona_list'),
     path('mascotas/', MascotaView.as_view(), name='mascota_list'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
